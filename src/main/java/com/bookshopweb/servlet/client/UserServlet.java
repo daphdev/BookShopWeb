0f4fe3ae141ccfc3b3c268323e9de782a9df4968
Nguyen Mau Thanh Hoang <80096974+joshuaNguyen6820@users.noreply.github.com>
2022-06-16 21:40:49 +0700

    Update UserServlet.java

--- src/main/java/com/bookshopweb/servlet/client/UserServlet.java
+++ src/main/java/com/bookshopweb/servlet/client/UserServlet.java
@@ -1,12 +1,11 @@
 package com.bookshopweb.servlet.client;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(name = "UserServlet", value = "/user")
-public class userServlet extends HttpServlet {
+public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("WEB-INF/views/userView.jsp").forward(request, response);
