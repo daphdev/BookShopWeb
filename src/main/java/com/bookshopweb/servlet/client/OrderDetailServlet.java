36e63a6b9a63cb82ab3fdaa3cadf524b2dcdd127
buithienhao-nlu <18130069@st.hcmuaf.edu.vn>
2022-08-02 22:22:36 +0700

    cm2

--- src/main/java/com/bookshopweb/servlet/client/OrderDetailServlet.java
+++ src/main/java/com/bookshopweb/servlet/client/OrderDetailServlet.java
@@ -1,14 +1,21 @@
 package com.bookshopweb.servlet.client;
 
+import com.bookshopweb.beans.User;
+import com.bookshopweb.dto.OrderResponse;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "OrderDetailServlet", value = "/OrderDetailServlet")
+@WebServlet(name = "OrderDetailServlet", value = "/orderDetail")
 public class OrderDetailServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = (User) request.getSession().getAttribute("currentUser");
+
+
+
         request.getRequestDispatcher("WEB-INF/views/orderDetailView.jsp").forward(request, response);
     }
 
